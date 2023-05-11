36322620e3d201d105a06f0aff7ddfe9a6f62d9b
ahadsts9901 <ahadsts990@gmail.com>
2023-05-11 11:41:41 -0700

    problem in shapes

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
 let board = document.getElementById("board");
 let color = document.getElementById("color");
 let size = document.getElementById("size");
-let save = document.getElementById("save")
+let save = document.getElementById("save");
+let line = document.getElementById("line");
+let circle = document.getElementById("circle");
+let rectangle = document.getElementById("rectangle");
 let context = board.getContext("2d");
 let isLine = false;
 let finalOffsetX = 0;
 let finalOffsetY = 0;
 
-board.addEventListener("mousedown", function (event) {
+// Set canvas width and height on window load
+window.addEventListener("load", () => {
+    board.width = board.offsetWidth;
+    board.height = board.offsetHeight;
+});
+
+board.addEventListener("mousedown", function(event) {
     isLine = true;
     finalOffsetX = event.offsetX;
     finalOffsetY = event.offsetY;
     // console.log(event)
 });
 
-board.addEventListener("mousemove", function (event) {
+board.addEventListener("mousemove", function(event) {
     if (isLine) {
-        context.beginPath();
-        context.moveTo(finalOffsetX, finalOffsetY);
-        context.lineTo(event.offsetX, event.offsetY);
-        context.stroke();
-        finalOffsetX = event.offsetX;
-        finalOffsetY = event.offsetY;
-        context.strokeStyle = color.value;
-        context.lineWidth = size.value;
+        if (line.checked) {
+            context.beginPath();
+            context.moveTo(finalOffsetX, finalOffsetY);
+            context.lineTo(event.offsetX, event.offsetY);
+            context.stroke();
+            finalOffsetX = event.offsetX;
+            finalOffsetY = event.offsetY;
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+        } else if (rectangle.checked) {
+            // clearBoard()
+            context.beginPath();
+            context.rect(
+                finalOffsetX,
+                finalOffsetY,
+                event.offsetX - finalOffsetX,
+                event.offsetY - finalOffsetY
+            );
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+            context.stroke();
+        } else if (circle.checked) {
+            // clearBoard()
+            context.beginPath();
+            let radius = Math.sqrt(
+                Math.pow(event.offsetX - finalOffsetX, 2) +
+                Math.pow(event.offsetY - finalOffsetY, 2)
+            );
+            context.arc(finalOffsetX, finalOffsetY, radius, 0, 2 * Math.PI);
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+            context.stroke();
+        }
     }
 });
 
-board.addEventListener("mouseup", function () {
+board.addEventListener("mouseup", function() {
     isLine = false;
 });
 
@@ -35,7 +69,7 @@ function clearBoard() {
     context.clearRect(0, 0, board.width, board.height);
 }
 
-board.addEventListener("touchstart", function (event) {
+board.addEventListener("touchstart", function(event) {
     event.preventDefault();
     isLine = true;
     let coordinates = getCoordinates(event.touches[0]);
@@ -43,22 +77,11 @@ board.addEventListener("touchstart", function (event) {
     finalOffsetY = coordinates.y;
 });
 
-board.addEventListener("touchmove", function (event) {
+board.addEventListener("touchmove", function(event) {
     event.preventDefault();
-    if (isLine) {
-        let coordinates = getCoordinates(event.touches[0]);
-        context.beginPath();
-        context.moveTo(finalOffsetX, finalOffsetY);
-        context.lineTo(coordinates.x, coordinates.y);
-        context.stroke();
-        finalOffsetX = coordinates.x;
-        finalOffsetY = coordinates.y;
-        context.strokeStyle = color.value;
-        context.lineWidth = size.value;
-    }
 });
 
-board.addEventListener("touchend", function () {
+board.addEventListener("touchend", function() {
     isLine = false;
 });
 
