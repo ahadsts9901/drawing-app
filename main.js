1a260d6b604c1aa9278ee002f8d9dab94bf263ea
ahadsts9901 <ahadsts990@gmail.com>
2023-05-12 10:50:11 -0700

    shapes done

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let board = document.getElementById("board");
+let fill = document.getElementById("shape-fill-check");
+let shapeFill = document.getElementById("shape-fill");
 let color = document.getElementById("color");
 let size = document.getElementById("size");
 let save = document.getElementById("save");
+let brush = document.getElementById("brush");
 let line = document.getElementById("line");
 let circle = document.getElementById("circle");
 let rectangle = document.getElementById("rectangle");
@@ -9,6 +12,7 @@ let context = board.getContext("2d");
 let isLine = false;
 let finalOffsetX = 0;
 let finalOffsetY = 0;
+brush.checked = true;
 
 // Set canvas width and height on window load
 window.addEventListener("load", () => {
@@ -25,7 +29,7 @@ board.addEventListener("mousedown", function(event) {
 
 board.addEventListener("mousemove", function(event) {
     if (isLine) {
-        if (line.checked) {
+        if (brush.checked) {
             context.beginPath();
             context.moveTo(finalOffsetX, finalOffsetY);
             context.lineTo(event.offsetX, event.offsetY);
@@ -37,7 +41,7 @@ board.addEventListener("mousemove", function(event) {
         } else if (rectangle.checked) {
             // clearBoard()
             context.beginPath();
-            context.rect(
+            context.strokeRect(
                 finalOffsetX,
                 finalOffsetY,
                 event.offsetX - finalOffsetX,
@@ -46,6 +50,16 @@ board.addEventListener("mousemove", function(event) {
             context.strokeStyle = color.value;
             context.lineWidth = size.value;
             context.stroke();
+            context.fillRect(
+                finalOffsetX,
+                finalOffsetY,
+                event.offsetX - finalOffsetX,
+                event.offsetY - finalOffsetY
+            );
+            context.fillStyle = "#fff";
+            if (fill.checked) {
+                context.fillStyle = shapeFill.value;
+            }
         } else if (circle.checked) {
             // clearBoard()
             context.beginPath();
@@ -57,11 +71,29 @@ board.addEventListener("mousemove", function(event) {
             context.strokeStyle = color.value;
             context.lineWidth = size.value;
             context.stroke();
+            context.fillStyle = "#fff";
+            context.fill();
+            if (fill.checked) {
+                context.fillStyle = shapeFill.value;
+                context.fill();
+            }
         }
     }
 });
 
-board.addEventListener("mouseup", function() {
+board.addEventListener("mouseup", function(event) {
+    if (isLine) {
+        if (line.checked) {
+            context.beginPath();
+            context.moveTo(finalOffsetX, finalOffsetY);
+            context.lineTo(event.offsetX, event.offsetY);
+            context.stroke();
+            finalOffsetX = event.offsetX;
+            finalOffsetY = event.offsetY;
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+        }
+    }
     isLine = false;
 });
 
@@ -79,6 +111,67 @@ board.addEventListener("touchstart", function(event) {
 
 board.addEventListener("touchmove", function(event) {
     event.preventDefault();
+    if (isLine) {
+        if (brush.checked) {
+            let coordinates = getCoordinates(event.touches[0]);
+            context.beginPath();
+            context.moveTo(finalOffsetX, finalOffsetY);
+            context.lineTo(coordinates.x, coordinates.y);
+            context.stroke();
+            finalOffsetX = coordinates.x;
+            finalOffsetY = coordinates.y;
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+            // } else if (line.checked) {
+            //     let coordinates = getCoordinates(event.touches[0]);
+            //     context.beginPath();
+            //     finalOffsetX = coordinates.x;
+            //     finalOffsetY = coordinates.y;
+            //     context.moveTo(finalOffsetX, finalOffsetY);
+            //     context.lineTo(coordinates.x, coordinates.y);
+            //     context.stroke();
+            //     context.strokeStyle = color.value;
+            //     context.lineWidth = size.value;
+        } else if (rectangle.checked) {
+            let coordinates = getCoordinates(event.touches[0]);
+            context.beginPath();
+            context.rect(
+                finalOffsetX,
+                finalOffsetY,
+                coordinates.x - finalOffsetX,
+                coordinates.y - finalOffsetY
+            );
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+            context.fillRect(
+                finalOffsetX,
+                finalOffsetY,
+                coordinates.x - finalOffsetX,
+                coordinates.y - finalOffsetY
+            );
+            context.fillStyle = "#fff";
+            context.stroke();
+            if (fill.checked) {
+                context.fillStyle = shapeFill.value;
+            }
+        } else if (circle.checked) {
+            let coordinates = getCoordinates(event.touches[0]);
+            context.beginPath();
+            let radius = Math.sqrt(
+                Math.pow(coordinates.x - finalOffsetX, 2) +
+                Math.pow(coordinates.y - finalOffsetY, 2)
+            );
+            context.arc(finalOffsetX, finalOffsetY, radius, 0, 2 * Math.PI);
+            context.strokeStyle = color.value;
+            context.lineWidth = size.value;
+            context.stroke();
+            context.fill();
+            context.fillStyle = "#fff";
+            if (fill.checked) {
+                context.fillStyle = shapeFill.value;
+            }
+        }
+    }
 });
 
 board.addEventListener("touchend", function() {
@@ -88,6 +181,14 @@ board.addEventListener("touchend", function() {
 //get by youtube help
 
 function getCoordinates(touch) {
+    let rect = board.getBoundingClientRect();
+    return {
+        x: touch.clientX - rect.left,
+        y: touch.clientY - rect.top,
+    };
+}
+
+function getCoordinatesShapes(touch) {
     let rect = board.getBoundingClientRect();
     return {
         x: touch.clientX - rect.left,
